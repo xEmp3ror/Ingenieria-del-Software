c5bbe36d1765f59bb47b5c6f8435170d1778abf3
Juan Jose Ropero Cerro <i82rocej@uco.es>
2019-11-27 13:18:22 +0100

    Arreglo de problemas

--- src/06Crear_cita/06Crear_cita.cpp
+++ src/06Crear_cita/06Crear_cita.cpp
@@ -1,6 +1,43 @@
+#include "06Crear_cita.hpp"
+#include <string>
+#include <iostream>
+#include <ctime>
+#include <cstdio>
 
-bool Crear_cita(list<Cita>*c)
+using namespace std;
+
+bool Crear_cita(list <Cita> *c)
 {
 	Cita aux;
-	
+	string name;
+	int num;
+	struct tm fyh;
+
+	cout<<"Introduza el nombre del paciente\n";
+	cin>>name;
+	cout<<"Introduza el numero telefonico del paciente\n";
+	cin>>num;
+	cout<<"Introduzca la hora de la cita\n";
+	cout<<"Hora";
+	cin>>fyh.tm_hour;
+	cout<<"Minuto";
+	cin>>fyh.tm_min;
+	cout<<"Introduzca el Dia,Mes,Año de la cita,en numero\n";
+	cout<<"Dia";
+	cin>>fyh.tm_mday;
+	cout<<"Mes";
+	cin>>fyh.tm_mon;
+	cout<<"Año";
+	cin>>fyh.tm_year;
+	//if(Buscarcita(c,name,num)==false)
+	//{
+		aux.setNombre(name);
+		aux.setTelefono(num);
+		aux.setFechayHora(fyh);
+		c->push_back(aux);
+		return true;
+	//}else
+//	{
+	//	return false;
+//	}
 }
--- src/06Crear_cita/06Crear_cita.hpp
+++ src/06Crear_cita/06Crear_cita.hpp
@@ -1,6 +1,9 @@
-#ifndef 06CREAR_CITA_H
-#define 06CREAR_CITA_H
+#ifndef CREAR_CITA_H
+#define CREAR_CITA_H
 
-bool Crear_cita(list<Cita> *c);
+#include "cita.hpp"
+#include <list>
+
+bool Crear_cita(list <Cita> *c);
 
 #endif
