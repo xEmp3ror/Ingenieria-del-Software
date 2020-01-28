d6f16578f939ee898a6acf6c80eaac2730d6be34
Juan Jose Ropero Cerro <i82rocej@uco.es>
2020-01-28 19:35:45 +0100

    Corregido la funcion Mostrar Lista Pacientes para su correcto funcionamiento

--- src/funciones/Mostrar_lista_pacientes.cpp
+++ src/funciones/Mostrar_lista_pacientes.cpp
@@ -10,9 +10,9 @@ void MostrarListaPacientes(list<Paciente> p)
 	list<Persona>::iterator it;
 	for(it=p.begin();it!=pacientes_.end();it++)
 	{
-		cout<<"Nombre "it->getNombrecompleto()<<"\n"
-		cout<<"Numero "it->getTelefono()<<"\n"
-		cout<<"e-Mail "it->getCorreo()<<"\n"
+		cout<<"Nombre: "it->getNombre()<<"   |   Apellidos: "it->getApellidos()<<"\n";
+		cout<<"Fecha de nacimiento "it->getFechaNacimiento()<<"   |   Edad:"it->getEdad()<<"\n";
+		cout<<"Telefono "it->getTelefono()<<"\n";
 		cout<<"Direccion "it->getDireccion()<<"\n\n";
 
 	}
